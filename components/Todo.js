989d79abea9369cb42c925c5fdbf50cff076a90d
Tracey Lee <traceylg43@gmail.com>
2025-10-12 20:47:31 -0400

    feet: task3, created the Todo Class, private_setEventListener and public getView

---
+++ components/Todo.js
@@ -0,0 +1,50 @@
+class Todo {
+  constructor(data, selector) {
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+  }
+  _setEventListener() {
+    this.todoDeleteBtn.addEventListener("click", () => {
+      this.todoElement.remove();
+    });
+
+    this.todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+      console.log(this._data.completed);
+    });
+  }
+
+  _generateChecboxEl() {
+    this.todoCheckboxEl = this.todoElement.querySelector(".todo__completed");
+    this.todoLabel = this.todoElement.querySelector(".todo__label");
+    this.todoCheckboxEl.checked = this._data.completed;
+    this.todoCheckboxEl.id = `todo-${this._data.id}`;
+    this.todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  getView() {
+    this.todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
+    const todoNameEl = this.todoElement.querySelector(".todo__name");
+    const todoDate = this.todoElement.querySelector(".todo__date");
+    this.todoDeleteBtn = this.todoElement.querySelector(".todo__delete-btn");
+
+    todoNameEl.textContent = this._data.name;
+
+    this.dueDate = new Date(this._data.date);
+    if (!isNaN(this.dueDate)) {
+      todoDate.textContent = `Due: ${this.dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    }
+
+    this._generateChecboxEl();
+    this._setEventListener();
+    return this.todoElement;
+  }
+}
+
+export default Todo;
