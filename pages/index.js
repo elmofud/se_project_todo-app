9db5c5e72dd8e841a3278969bebf5f6d1c4016c5
Tracey Lee <traceylg43@gmail.com>
2025-10-13 10:51:41 -0400

    feet: adding unique IDs new todo item using uuidv4()

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+console.log(uuidv4());
+
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 
@@ -5,7 +8,7 @@ const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-//const todoTemplate = document.querySelector("#todo-template");
+
 const todosList = document.querySelector(".todos__list");
 
 const openModal = (modal) => {
@@ -15,6 +18,11 @@ const openModal = (modal) => {
 const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
+/*const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
+  const todoElement = todo.getView();
+  return todoElement;
+};*/
 
 addTodoButton.addEventListener("click", () => {
   openModal(addTodoPopup);
@@ -33,7 +41,8 @@ addTodoForm.addEventListener("submit", (evt) => {
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
-  const values = { name, date };
+  const id = uuidv4();
+  const values = { name, date, id };
   const todo = new Todo(values, "#todo-template");
   const todoElement = todo.getView();
 
